158b173625825d556d0c6d97425e71f676233c58
SamyoFox <samyocord@protonmail.com>
2021-12-04 23:04:20 +0100

    new website

--- js/checkDNT.js
+++
@@ -1,5 +0,0 @@
-if (window.doNotTrack || navigator.doNotTrack || navigator.msDoNotTrack || 'msTrackingProtectionEnabled' in window.external) {
-  if (window.doNotTrack == "1" || navigator.doNotTrack == "yes" || navigator.doNotTrack == "1" || navigator.msDoNotTrack == "1" || window.external.msTrackingProtectionEnabled()) {
-    document.getElementById("dnt-notice").style = "display: initial;";
-  } else {}
-} else {}
